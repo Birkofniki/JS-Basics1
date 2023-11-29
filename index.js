01cef3e403feea7e876607bc7d695ca9466e8ba2
BirkofNiki <augastinendeti@gmail.com>
2023-11-29 17:59:25 +0300

    my js learning with comments

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Hello world"); //This is a statement that expresses an action to be carried out...i.e,for this we want to log a message 'hello world'(the message is a string) in the console,
+    
